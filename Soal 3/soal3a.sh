95596d3a514a0b04edbe894e7cff19928f4004f5
adjiemahesa <55140514+adjiemahesa@users.noreply.github.com>
2021-03-27 20:42:42 +0700

    Update soal3a.sh

--- Soal 3/soal3a.sh
+++ Soal 3/soal3a.sh
@@ -1,56 +1,34 @@
 #!/bin/bash
 
 ## counter
-limit=23
+count=23
 
 ## looping
-for ((num=1;num<=limit;num=num+1))
+for ((iter=1;iter<=count;iter=iter+1))
 do
 
 ## penamaan file
-x=$(printf "%02d" $num)
+x=$(printf "%02d" $iter)
 
 ## download file + overwrite log
 wget -o Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_$x"
 
 ## mencari line untuk di compare
 temp=$(awk 'NR==6{print $3}' Foto.log) 
-echo $temp
-
-array[$num]="$(md5sum Koleksi_$x | awk 'NR==6{print $3}' Foto.log)"
-
-iter=num
-for ((iter=num-1;iter>=1;iter=iter-1))
-do
-##	temp2=$(awk 'NR==6{print $3}' Foto.log)
-	
-	if [[ "${array[$num]}" == "${array[$iter]}" ]]
-	then
-		rm -f "Koleksi_$x"
-		limit=$((limit-1))
-		num=$((num-1))
-	fi
-done
-
-
-
-
-##temp2=$(awk -v check="$temp" ' 
-##BEGIN{count=0}
-##{if( $0-check )count++} END{print count}' Foto.log)
-##echo $temp2
-
-##if [[ $temp2>0 ]]
-##then
-##rm -f "Koleksi_$x"
-##fi
-
-
-##array[$num]="$temp"
-
 ##echo $temp
-##echo "${array[$num]}"
-##rm -f "Koleksi_$x"
 
+a=num
+for ((a=iter-1;a>=1;a=a-1))
+do
+        wget -o Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_$x"
+        temp2=$(awk 'NR==6{print $3}' Foto.log)
+
+        if [[ "$temp" == "$temp2" ]]
+        then
+                rm -f "Koleksi_$x"
+                count=$((count-1))
+                iter=$((iter-1))
+        fi
 done
 
+done
