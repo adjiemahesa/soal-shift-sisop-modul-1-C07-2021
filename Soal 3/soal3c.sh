109b9c1abe6d78fa5c0537d77b27378a9ba94963
adjiemahesa <55140514+adjiemahesa@users.noreply.github.com>
2021-04-04 19:08:02 +0700

    Revisi Soal 3C

--- Soal 3/soal3c.sh
+++ Soal 3/soal3c.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 limit=23
-x=$(date "+%--j")
+x=$(date +"%--j")
+day=$(date '+%d-%m-%Y')
 mod=$((x % 2))
+echo $x
+echo $mod
 
-
-if [[ $mod -eq  0 ]]
+if [ ! -d "Kucing_$day" ]&&[ $mod -eq  0 ]
 then
-
 for ((num=1;num<=23;num=num+1))
 do
-	i=$(printf "%02d" $num)
-	$(mkdir "Kucing_"$(date +"%d-%m-%Y"))
-	wget -O /home/adjie/Kucing_$(date +"%d-%m-%Y")/Koleksi_$i 'https://loremflickr.com/320/240/kitten'
+        i=$(printf "%02d" $num)
+        mkdir -p "Kucing_$day"
+        wget -O /home/adjie/Kucing_$(date +"%d-%m-%Y")/Koleksi_$i 'https://loremflickr.com/320/240/kitten'
+
 done
 
-elif [[ $mod -eq 1 ]]
+elif [ ! -d "Kelinci_$day" ]&&[ $mod -ne 0 ]
 then
-
 for ((num=1;num<=23;num=num+1))
 do
-	i=$(printf "%02d" $num)
-	$(mkdir Kelinci_$(date +"%d-%m-%Y"))
-	wget -O /home/adjie/Kelinci_$(date +"%d-%m-%Y")/Koleksi_$i 'https://loremflickr.com/320/240/bunny'
+        i=$(printf "%02d" $num)
+        mkdir -p "Kelinci_$day"
+        wget -O /home/adjie/Kelinci_$(date +"%d-%m-%Y")/Koleksi_$i 'https://loremflickr.com/320/240/bunny'
+
 done
 fi
+
+
+
