51b63eb7e03ea460662a1c28c178c0f7e303688e
adjiemahesa <55140514+adjiemahesa@users.noreply.github.com>
2021-04-04 19:05:25 +0700

    Update soal3a.sh
    revisi soal3a

--- Soal 3/soal3a.sh
+++ Soal 3/soal3a.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-## counter
-count=23
+##declare array
+array=()
 
 ## looping
-for ((iter=1;iter<=count;iter=iter+1))
+for ((iter=1;iter<=23;iter=iter+1))
 do
 
 ## penamaan file
 x=$(printf "%02d" $iter)
 
 ## download file + overwrite log
-wget -o Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_$x"
+wget -O "Koleksi_$x" -o Foto.log https://loremflickr.com/320/240/kitten 
 
-## mencari line untuk di compare
-temp=$(awk 'NR==6{print $3}' Foto.log) 
-##echo $temp
+array[$iter]="$(md5sum Koleksi_$x | awk '{print $1}')"
+echo "${array[$iter]}"
 
-a=num
-for ((a=iter-1;a>=1;a=a-1))
+for ((a=1;a<iter;a=a+1))
 do
-        wget -o Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_$x"
-        temp2=$(awk 'NR==6{print $3}' Foto.log)
-
-        if [[ "$temp" == "$temp2" ]]
-        then
-                rm -f "Koleksi_$x"
-                count=$((count-1))
-                iter=$((iter-1))
+if [[ "${array[$iter]}" ==  "${array[$a]}" ]];
+then 
+        echo "Delete Duplikat"
+        rm -f "Koleksi_$x"
+        iter=$((iter-1))
         fi
 done
 
 done
+
+
